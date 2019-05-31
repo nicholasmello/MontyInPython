de45cf50bae313b0a3ce0287304a0d6e25a3fdbd
xonmello <43194417+xonmello@users.noreply.github.com>
2019-05-31 16:20:09 -0600

    Added comments to the states and
    Additionally, I added a print error when "wait" state is reached. The states should never go past "towardball" state since it is a catch.

--- monty/States.py
+++ monty/States.py
@@ -4,7 +4,7 @@ from rlbot.agents.base_agent import  SimpleControllerState
 from Util import *
 from enum import Enum, auto
 
-
+''' KICKOFF STATE '''
 class kickoff:
     def __init__(self):
         self.expired = False
@@ -16,17 +16,19 @@ class kickoff:
         agent.controller = kickoffController
 
         if agent.ball.location.data[0] != 0 and agent.ball.location.data[1] != 0:
-            self.expired = True
+            self.expired = True # <-- Expired condition is when the ball is no longer in the center of the field for the first time after the round becomes active
         return agent.controller(agent)
         
 def kickoffController(agent):
     controller_state = SimpleControllerState()
-    localBall = toLocal(agent.ball, agent.me)
-    controller_state.throttle = 1
-    flipCar(agent, controller_state, flipDirection.FORWARD)
-
+    localBall = toLocal(agent.ball, agent.me) # <-- Currently unused. Will be used later.
+    controller_state.throttle = 1 # <-- Drive Forward.
+    
+    flipCar(agent, controller_state, flipDirection.FORWARD) # <-- Flip the car in the forward direction.
+    ''' Rest of kickoff will go here. '''
     return controller_state
 
+''' TOWARD BALL STATE '''
 class towardball:
     def __init__(self):
         self.expired = False
@@ -36,7 +38,7 @@ class towardball:
         agent.controller = towardballController
         target_location = agent.ball
         target_speed = velocity2D(agent.ball) + (distance2D(agent.ball,agent.me)/1.5)
-        
+        self.expired = True
         return agent.controller(agent, target_location, target_speed)
 
 def towardballController(agent, target_object,target_speed):
@@ -58,6 +60,8 @@ def towardballController(agent, target_object,target_speed):
 
     return controller_state
 
+
+''' WAIT STATE '''
 class wait():
     def __init__(self):
         self.expired = False
@@ -65,8 +69,7 @@ class wait():
         if timeZ(agent.ball) > 2:
             return True
     def execute(self,agent):
-        
         self.expired = True
         controller_state = SimpleControllerState()
-
+        print("Error: All states bypassed to wait state.")
         return controller_state
